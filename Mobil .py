6d2a7788572a6b472a6648b75d67cca1ea437c39
Dillanurfadilla <127158470+Dillanurfadilla@users.noreply.github.com>
2023-04-16 23:46:43 +0700

    Delete Mobil .py

--- Mobil .py
+++
@@ -1,8 +0,0 @@
-class Mobil:
-   def __init__(self, merk, warna):
-       self.merk = merk
-       self.warna = warna
-   def info(self):
-    print(f"Mobil {self.merk} berwana {self.warna}")
-MobilA = Mobil("Toyota", "Hitam")
-MobilA.info() # Output: Mobil Toyota berwarna hitam
